a146e36f69fda5e0e2cb6cd68e8f30acabf9a977
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-10-03 23:31:24 -0500

    second Commit

--- src/auth/account/resolvers.js
+++ src/auth/account/resolvers.js
@@ -4,12 +4,7 @@ import { url, port, entryPoint } from './server';
 const URL = `http://${url}:${port}/${entryPoint}`;
 
 const resolvers = {
-	Query: {
-		allCategories: (_) =>
-			getRequest(URL, ''),
-		categoryById: (_, { id }) =>
-			generalRequest(`${URL}/${id}`, 'GET'),
-	},
+	
 	Mutation: {
 		createAccount: (_, { account }) =>
 			generalRequest(`${URL}/`, 'POST', account),
